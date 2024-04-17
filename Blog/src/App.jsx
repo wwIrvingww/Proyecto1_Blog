f9284c142376ead870998caa04839fe6171530c9
wwIrvingww <mor22781@uvg.edu.gt>
2024-04-17 16:08:55 -0600

    Home, Login, Register and Single

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,11 +1,13 @@
 import TopBar from "./components/TopBar/TopBar";
-import Home from "./Pages/Home";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
   <>
     <TopBar />
-    <Home />
+    <Register />
   </>
   );
 }
--- Blog/src/Pages/Home/Home.jsx
+++ Blog/src/Pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 import './home.css';
-import Header from '../components/Header/Header';
-import Posts from '../components/Posts/Posts';
-import Sidebar from '../components/Sidebar/Sidebar';
+import Header from "../../components/Header/Header";
+import Posts from '../../components/Posts/Posts';
+import Sidebar from '../../components/Sidebar/Sidebar';
+
 export default function Home() {
     return(
     <>    
