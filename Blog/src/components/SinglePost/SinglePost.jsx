460cb4025cf2e6ea2737a002c4322c381b2d9371
wwIrvingww <mor22781@uvg.edu.gt>
2024-04-23 08:19:50 -0600

    input to change the image added

--- Blog/src/components/SinglePost/SinglePost.jsx
+++ Blog/src/components/SinglePost/SinglePost.jsx
@@ -2,24 +2,50 @@ import './singlePost.css'
 import { useEffect, useState } from 'react'
 
 export default function SinglePost({ postId }) {
-    const [post, setPost] = useState(null);
-    const [isEditing, setIsEditing] = useState(false);
-    const [editedTitle, setEditedTitle] = useState('');
-    const [editedContent, setEditedContent] = useState('');
-
+    //CREAR POST EN SINGLE
+    const [post, setPost] = useState(null)
+    const [isEditing, setIsEditing] = useState(false)
+    const [editedTitle, setEditedTitle] = useState('')
+    const [editedContent, setEditedContent] = useState('')
+    const [editedImage64, setEditedImage64] = useState('')
     useEffect(() => {
         const fetchPost = async () => {
             try {
-                const response = await fetch(`http://127.0.0.1:3001/blogs/${postId}`);
-                const postData = await response.json();
+                const response = await fetch(`http://127.0.0.1:3001/blogs/${postId}`)
+                const postData = await response.json()
                 console.log('Post Data:', postData); 
-                setPost(postData);
+              setPost(postData)
+                setEditedTitle(postData[0].title)
+                setEditedContent(postData[0].content)
             } catch (error) {
-                console.error('Error fetching post:', error);
+                console.error('Error fetching post:', error)
             }
-        };
-        fetchPost();
-    }, [postId]);
+        }
+        fetchPost()
+    }, [postId])
+
+    if (!post) {
+        return <h1>Loading...</h1>
+    }
+
+    //PARA ELIMINAR POST 
+    const handleItemClick = (route) => {
+        onRouteChange(route)
+    }
+    const deletePost = async (id, onRouteChange) => {
+        let response = await fetch(`http://127.0.0.1:3001/blogs/${postId}`,
+        {
+            method: 'DELETE',
+        }
+    );
+    if (response.status === 200){
+        console.log('Post deleted successfully');
+        
+    } else {
+        return
+        console.log('error to delete post')
+    }
+    }
 
     const editPost = async () => {
         try {
@@ -31,6 +57,7 @@ export default function SinglePost({ postId }) {
                 body: JSON.stringify({
                     title: editedTitle,
                     content: editedContent,
+                    image64: editedImage64
                 }),
             });
             const updatedPostData = await response.json();
@@ -49,23 +76,30 @@ export default function SinglePost({ postId }) {
     return(   
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img src={post[0].image64} alt="" className="singlePostImg" />
+                <img src={post[0].image64} 
+                alt="" 
+                className="singlePostImg"
+                />
                 {isEditing ? (
                     <>
                         <input
                             type="text"
-                            placeholder="New Title"
-                            className="writeInput"
-                            autoFocus={true}
                             value={editedTitle}
-                            onChange={(e) => setEditedTitle(e.target.value)}/>
+                            onChange={(e) => setEditedTitle(e.target.value)}
+                            className="singlePostTitleInput"
+                        />
                         <textarea
-                            placeholder="New Content"
-                            type="text"
-                            className="writeInput writeText"
                             value={editedContent}
-                            onChange={(e) => setEditedContent(e.target.value)}>
-                        </textarea>
+                            onChange={(e) => setEditedContent(e.target.value)}
+                            className="singlePostDescInput"
+                        />
+                        <textarea
+                            value={editedImage64}
+                            onChange={(e) => setEditedImage64(e.target.value)}
+                            className="singlePostImageInput"
+                        />
+
+                        <button onClick={editPost} className="singlePostButton">Submit</button>
                     </>
                 ) : (
                     <>
@@ -73,20 +107,17 @@ export default function SinglePost({ postId }) {
                             {post[0].title} 
                             <div className="singlePostEdit">
                                 <i className="singlePostIcon fa-regular fa-pen-to-square" onClick={() => setIsEditing(true)}></i>
-                                <i className="singlePostIcon fa-regular fa-trash-can" onClick={() => deletePost(postId)}></i> 
+                                <i className="singlePostIcon fa-regular fa-trash-can" onClick={() => deletePost(postId) }></i> 
                             </div>
                         </h1>
                         <div className="singlePostInfo">
                             <span className='singlePostAuthor'>Autor: <b>IRVS</b> </span>
                             <span className='singlePostDate'>Date: {post[0].date} </span> 
                         </div>
-                        <p className='singlePostDesc'>{post[0].content}</p>
+                        <p className='singlePostDesc'>{post[0].content}</p> 
                     </>
                 )}
             </div>
-            {isEditing && (
-                <button className="writeSubmit" onClick={editPost}>Save</button>
-            )}
         </div>
-    );
+    )
 }
